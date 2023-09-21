fa78a1a0c2adcac6c62d1b8ba25de6d2789ea705
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-09-20 21:06:55 -0500

    project setup

--- Games/platformer/index.js
+++ Games/platformer/index.js
@@ -1,12 +1,16 @@
+//project setup
+//objects
 const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 
+console.log(c)
 //gamescreen
 canvas.width=1024
 canvas.height=576
 c.fillStyle = "white"
-c.fillRect(0,0,canvas.width,canvas.height)
+c.fillRect(0,0,canvas.width,canvas.height)//x=0 y=0 to start at top corner
 
 //player
 c.fillStyle = "red"
 c.fillRect(200, 100, 100, 100)
+
