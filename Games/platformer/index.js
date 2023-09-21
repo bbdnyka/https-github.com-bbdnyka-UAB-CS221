b046977366c6645bfb2e3ae702741ca914022edd
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-09-20 20:47:14 -0500

    player

--- Games/platformer/index.js
+++ Games/platformer/index.js
@@ -1,7 +1,12 @@
 const canvas = document.querySelector("canvas")
 const c = canvas.getContext("2d")
 
+//gamescreen
 canvas.width=1024
 canvas.height=576
 c.fillStyle = "white"
 c.fillRect(0,0,canvas.width,canvas.height)
+
+//player
+c.fillStyle = "red"
+c.fillRect(200, 100, 100, 100)
