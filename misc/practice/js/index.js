392381359437b56f0fd05185e78f111ef4596e37
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-04 10:49:56 -0500

    1 file modified

--- misc/practice/js/index.js
+++ misc/practice/js/index.js
@@ -2,3 +2,5 @@
 const canvas = document.querySelector("canvas")
 const firstname=document.getElementById("firstName")
 const lastName=document.getElementById("lastName")
+console.log(firstname)
+console.log(lastName)
