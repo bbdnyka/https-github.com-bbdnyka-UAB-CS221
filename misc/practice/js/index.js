465e824aa0e705dc1ff2d6e51c4f2a37846222a9
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-04 15:07:19 -0500

    2 files modified

--- misc/practice/js/index.js
+++ misc/practice/js/index.js
@@ -3,19 +3,20 @@ let loginForm = document.getElementById("loginForm");
 loginForm.addEventListener("submit", (e) => {
   e.preventDefault();
 
-  let fname = document.getElementById("firstNamw");
-  let lname = document.getElementById("lastName");
+  let username = document.getElementById("username");
+  let password = document.getElementById("password");
 
-  if (fname.value == "" || lname.value == "") {
+  if (username.value == "" || password.value == "") {
     alert("Ensure you input a value in both fields!");
   } else {
     // perform operation with form input
     alert("This form has been successfully submitted!");
     console.log(
-      `This form has a username of ${fname.value} and password of ${lname.value}`
+      `This form has a username of ${username.value} and password of ${password.value}`
     );
 
-    fname.value = "";
-    lname.value = "";
+    username.value = "";
+    password.value = "";
   }
 });
+
