e77bbd011c255a63ae4949b17d5a74a524772266
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-21 18:16:51 -0500

    switch case

--- Homework/HW8/js/browden2.hw8.js
+++ Homework/HW8/js/browden2.hw8.js
@@ -7,18 +7,18 @@ switch (true) {
     text = "Your grade: A.n/Wow. You're actually smart.";
     break;
   case enterGrade >= 80:
-    text = 'Good job, I guess.';
+    text = 'Your grade: B.n/Good job, I guess.';
     break;
   case enterGrade >= 70:
-    text = "Almost didn't make it there.";
+    text = "Your grade: C.n/Almost didn't make it there.";
     break;
   case enterGrade >= 60:
-    text = 'You need to work harder.';
+    text = 'Your grade: D.n/You need to work harder.';
     break;
   case enterGrade < 60:
-    text = 'Mmight be hard to come back from this one.';
+    text = 'Your grade: F.n/Mmight be hard to come back from this one.';
     break;
   default:
-    text = 'Insufficient value';
+    text = 'Insufficient value. Please use numbers between 1-100.';
 }
 console.log(text);
