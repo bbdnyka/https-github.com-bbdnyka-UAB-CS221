503f18daac20097e46f3695ff330f5f5789f5341
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-21 18:10:42 -0500

    1 file modified

--- Homework/HW8/js/browden2.hw8.js
+++ Homework/HW8/js/browden2.hw8.js
@@ -1,22 +1,24 @@
 let text;
-let yourGrade=prompt("Enter the grade here: ","A");
-switch(yourGrade){
-  case "A":
-    text="Wow. You're actually smart.";
+let grade;
+let enterGrade = prompt('Enter grade in a number format here: ', '100');
+
+switch (true) {
+  case enterGrade >= 90:
+    text = "Your grade: A.n/Wow. You're actually smart.";
     break;
-  case "B":
-    text="Good job, I guess.";
+  case enterGrade >= 80:
+    text = 'Good job, I guess.';
     break;
-  case "C":
-    text="Almost didn't make it there.";
+  case enterGrade >= 70:
+    text = "Almost didn't make it there.";
     break;
-  case "D":
-    text="You need to work harder.";
+  case enterGrade >= 60:
+    text = 'You need to work harder.';
     break;
-  case "F":
-    text="Mmight be hard to come back from this one.";
+  case enterGrade < 60:
+    text = 'Mmight be hard to come back from this one.';
     break;
   default:
-    text="Insufficient value";
+    text = 'Insufficient value';
 }
 console.log(text);
