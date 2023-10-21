423102d704f1a4decf0fd23fc89f829d4e706fc5
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-21 17:44:29 -0500

    hw8

---
+++ Homework/HW8/js/browden2.hw8.js
@@ -0,0 +1,19 @@
+let text;
+let yourGrade=prompt("Enter the grade here: ");
+switch(yourGrade){
+  case "A":
+    text="Wow. You're actually smart.";
+    break;
+  case "B":
+    text="Good job, I guess.";
+    break;
+  case "C":
+    text="Almost didn't make it there.";
+    break;
+  case "D":
+    text="You need to work harder.";
+    break;
+  case "F":
+    text="Mmight be hard to come back from this one.";
+    break;
+}
