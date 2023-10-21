dc952175235a1e6ea4e69e8e964fc1b98f8d6c70
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-21 18:30:44 -0500

    1 file modified

--- Homework/HW8/js/browden2.hw8.js
+++ Homework/HW8/js/browden2.hw8.js
@@ -1,24 +1,24 @@
 let text;
-let grade;
+
 let enterGrade = prompt('Enter grade in a number format here: ', 'i.e. 50');
 
 switch (true) {
   case enterGrade >= 90:
-    text = "Your grade: A. Here's some brocolli.";
+    text = 'Your grade: A. Your treat: Steamed broccoli.';
     break;
   case enterGrade >= 80:
-    text = "Your grade: B. Here's a carrot.";
+    text = 'Your grade: B. Your treat: A carrot.';
     break;
   case enterGrade >= 70:
-    text = 'Your grade: C. Baked shoe smothered in laces.';
+    text = 'Your grade: C. Your treat: Baked shoe smothered in laces.';
     break;
   case enterGrade >= 60:
-    text = 'Your grade: D. Deep fried cockroach.';
+    text = 'Your grade: D. Your treat: Deep fried cockroach.';
     break;
   case enterGrade < 60:
-    text = 'Your grade: F. Dirt.';
+    text = "Your grade: F. Your treat: Dirt. You don't deserve anything.";
     break;
   default:
-    text = 'Insufficient value. Please use numbers between 1-100.', enterGrade;
+    text = 'Insufficient value. Please reload and use numbers between 1-100.';
 }
 console.log(text);
