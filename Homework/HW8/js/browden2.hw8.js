740407430d6936b521334d979f960384e4de8064
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-21 19:13:41 -0500

    1 file modified

--- Homework/HW8/js/browden2.hw8.js
+++ Homework/HW8/js/browden2.hw8.js
@@ -1,24 +1,46 @@
-let text;
+let text; //variable for holding error message
+let grade = 'Your grade: '; //variable for holding grade value
+let letterGrade = 'Your letter grade: '; //variable for holding grade letter
+let treat = 'Your treat: '; //variable for holding treat
 
+//prompt
 let enterGrade = prompt('Enter grade in a number format here: ', 'i.e. 50');
 
-switch (true) {
-  case enterGrade >= 90:
-    text = 'Your grade: A. Your treat: Steamed broccoli.';
+switch (
+  true //switch case for calculating/translating grades and issuing treats
+) {
+  case enterGrade >= 90 && enterGrade < 100: //grade: 90-100
+    grade += enterGrade.toString();
+    letterGrade += 'A.';
+    treat += 'broccoli.';
+    console.log(grade + '.' + '\n' + letterGrade + '\n' + treat);
     break;
-  case enterGrade >= 80:
-    text = 'Your grade: B. Your treat: A carrot.';
+  case enterGrade >= 80 && enterGrade < 90: //grade: 80-89
+    grade += enterGrade.toString();
+    letterGrade += 'B.';
+    treat += 'milk';
+    console.log(grade + '.' + '\n' + letterGrade + '\n' + treat);
     break;
-  case enterGrade >= 70:
-    text = 'Your grade: C. Your treat: Baked shoe smothered in laces.';
+  case enterGrade >= 70 && enterGrade < 80: //grade: 70-79
+    grade += enterGrade.toString();
+    letterGrade += 'C.';
+    treat += 'something edible.';
+    console.log(grade + '.' + '\n' + letterGrade + '\n' + treat);
     break;
-  case enterGrade >= 60:
-    text = 'Your grade: D. Your treat: Deep fried cockroach.';
+  case enterGrade >= 60 && enterGrade < 70: //grade: 60-69
+    grade += enterGrade.toString();
+    letterGrade += 'D.';
+    treat += 'You deserve nothing. ';
+    console.log(grade + '.' + '\n' + letterGrade + '\n' + treat);
     break;
-  case enterGrade < 60:
-    text = "Your grade: F. Your treat: Dirt. You don't deserve anything.";
+  case enterGrade < 60 && enterGrade >= 0: //grade: 0-59
+    grade += enterGrade.toString();
+    letterGrade += 'F.';
+    treat += 'Air.';
+    console.log(grade + '.' + '\n' + letterGrade + '\n' + treat);
     break;
   default:
-    text = 'Insufficient value. Please reload and use a number between 1-100.';
+    //for input errors. Number not between 1-100, letters entered, more than one number,  etc.
+    text = 'Insufficient value. Please use numbers between 1-100.';
+    console.log(text);
 }
-console.log(text);
