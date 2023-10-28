9611a1fc85d1b638b2a58bdb43f812d838f24faf
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-10-28 14:21:49 -0500

    game objective1 file modified

--- lab9/js/browden2.lab9.js
+++ lab9/js/browden2.lab9.js
@@ -1,6 +1,13 @@
+
+//Deciding a winner : scissors beats paper, rock beats scissors, paper beats rock
+
 let rock; 
 let paper;
 let scissors;
+const computerOpt = ['rock', 'paper', 'scissors'];
 
-//let enterChoice=prompt("Enter your choice. Rock, Paper, or Scissors.", Rock, Paper, or Scissors)
+//let playerChoice = prompt('Enter your choice: rock, paper, or scissors?', 'Enter here');
 
+function {
+  
+}
