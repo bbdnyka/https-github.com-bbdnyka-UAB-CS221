b439ed89f9eb1f5af68900bee80f5fd4d8a26790
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-01 11:28:16 -0500

    1 file modified

--- lab9/js/browden2.lab9.js
+++ lab9/js/browden2.lab9.js
@@ -12,8 +12,8 @@ function playersPlay() {
   //player enters choice here
   let playerSelection = prompt('Enter Rock, Paper, or Scissors: ');
   while (playerSelection == null || playerSelection == '') {
-    playerSelection = prompt('Enter Rock, Paper, or Scissors: ');
     console.log('invalid input');
+    return playerSelection = prompt('Enter Rock, Paper, or Scissors: ');
   }
 
   return playerSelection;
