abd267ba1511350a2ba0ca65749b515b293e9cbe
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-01 10:59:10 -0500

    added error handling

--- lab9/js/browden2.lab9.js
+++ lab9/js/browden2.lab9.js
@@ -1,58 +1,67 @@
-
 //Deciding a winner : scissors beats paper, rock beats scissors, paper beats rock
 //Reload browser to play again
 
 //Chooses a random play for computer
-function computerPlay(){
+function computerPlay() {
   const computerOpt = ['rock', 'paper', 'scissors'];
-  const random = Math.floor(Math.random()*computerOpt.length);
-  return(random, computerOpt[random]);
+  const random = Math.floor(Math.random() * computerOpt.length);
+  return random, computerOpt[random];
 }
-function playersPlay(){
+//error handling
+function playersPlay() {
+  //player enters choice here
+  let playerSelection = prompt('Enter Rock, Paper, or Scissors: ');
 
+  
+
+  while(playerSelection == null || 
+    playerSelection == ""||
+    playerSelection != 'rock' ||
+    playerSelection != 'paper' ||
+    playerSelection != 'scissors'){
+
+    console.log("invalid input");
+  }
+  playerSelection = playerSelection.toLowerCase();
 }
+
 //Decides the winner of each round. Function called in game()
 function playRound(playerSelection, computerSelection) {
-  playerSelection = playerSelection.toLowerCase();
+  
+  playerSelection=playersPlay();
 
   //statement for deciding winner of each round
   if (playerSelection === computerSelection) {
     return "It's a tie!";
   } else if (
-    (playerSelection === "rock" && computerSelection === "scissors") ||
-    (playerSelection === "paper" && computerSelection === "rock") ||
-    (playerSelection === "scissors" && computerSelection === "paper")
+    (playerSelection === 'rock' && computerSelection === 'scissors') ||
+    (playerSelection === 'paper' && computerSelection === 'rock') ||
+    (playerSelection === 'scissors' && computerSelection === 'paper')
   ) {
-    return "You win!";
-  } 
-  else {
-    return "You lose!";
+    return 'You win!';
+  } else {
+    return 'You lose!';
   }
 }
 
 function game() {
   //variables increment based on decided winner of each round
-  let playerScore = 0; 
+  let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0; i < 1; i++) {// for loop manages how many rounds each game has. Choices a taken in succession(one after the other) and scores are calculated at the end
+  for (let i = 0; i < 1; i++) {
+    // for loop manages how many rounds each game has. Choices a taken in succession(one after the other) and scores are calculated at the end
 
-    //player enters choice here
-    const playerSelection = prompt("Enter Rock, Paper, or Scissors: ");
-
-    //error handling
-    if(playerSelection === null || playerSelection === ''){
-      console.log('No option entered');
-      break;
-    }
-    const computerSelection = computerPlay();//variable holds random element chosen from computerPlayer() 
+    
+    const playerSelection = playersPlay();//calls function to get players answer
+    const computerSelection = computerPlay(); //variable holds random element chosen from computerPlayer()
 
     //increases playerScore or computerScores based on decision from playRound()
-    playRound(playerSelection, computerSelection); 
+    playRound(playerSelection, computerSelection);
 
-    if (playRound(playerSelection, computerSelection).includes("win")) {
+    if (playRound(playerSelection, computerSelection).includes('win')) {
       playerScore++;
-    } else if (playRound(playerSelection, computerSelection).includes("lose")) {
+    } else if (playRound(playerSelection, computerSelection).includes('lose')) {
       computerScore++;
     }
     //Outputs the player's and computer's plays and respective scores
@@ -60,12 +69,11 @@ function game() {
     console.log('Computer entered: ' + computerSelection);
   }
 
-  
   //decides final winner based on playerScore and computerScore
   if (playerScore > computerScore) {
-    console.log("You win the game!");
+    console.log('You win the game!');
   } else if (computerScore > playerScore) {
-    console.log("Computer wins the game!");
+    console.log('Computer wins the game!');
   } else {
     console.log("It's a tie game!");
   }
