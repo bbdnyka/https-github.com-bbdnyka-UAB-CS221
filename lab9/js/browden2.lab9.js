fc24d0484fea3fca86ccacd34c52dbbc65306471
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-01 12:36:27 -0500

    added error handling

--- lab9/js/browden2.lab9.js
+++ lab9/js/browden2.lab9.js
@@ -13,7 +13,7 @@ function playersPlay() {
   let playerSelection = prompt('Enter Rock, Paper, or Scissors: ');
 
   if (playerSelection === null) {
-    // Player clicked "Cancel" . If more than 1 round played, have to cancel for each round
+    // Player clicked "Cancel" .
     alert('You canceled the input.');
     return playerSelection;
   } else {
@@ -53,7 +53,7 @@ function game() {
   let playerScore = 0;
   let computerScore = 0;
 
-  for (let i = 0; i < 1; i++) {
+  for (let i = 0; i < 3; i++) {
     // for loop manages how many rounds each game has. Choices a taken in succession(one after the other) and scores are calculated at the end
 
     //game ends if players clicks "Cancel"
