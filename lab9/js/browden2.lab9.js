8f44b1a1cdcc698c3ab5afc66ffb0298d4a3c8ac
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-08 17:26:23 -0600

    1 file modified

--- lab9/js/browden2.lab9.js
+++ lab9/js/browden2.lab9.js
@@ -66,11 +66,9 @@ function game() {
   let playerScore = 0;
   let computerScore = 0;
   
+  for (let i = 0; i < 3; i++) {//rounds of the game
 
-  for (let i = 0; i < 3; i++) {
-    roundCount += 1;
-    //Manages how many rounds each game has. Choices a taken in succession(one after the other) and scores are calculated at the end
-
+    roundCount += 1;//rounds counted here
     const playerSelection = playersPlay(); //calls function to get players answer
     const computerSelection = computerPlay(); //variable holds random element chosen from computerPlayer()
 
