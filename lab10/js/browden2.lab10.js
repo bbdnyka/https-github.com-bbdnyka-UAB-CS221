3fe16414810a85f69dadface63cba300d416b1b8
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-09 17:17:53 -0600

    1 file modified

--- lab10/js/browden2.lab10.js
+++ lab10/js/browden2.lab10.js
@@ -1,15 +1,15 @@
 const rockButton = 
 document.getElementById("rock")
-rockButton.addEventListener("Click", () => {
-  console.log("Rock button clicked")
+rockButton.addEventListener("click", () => {
+  console.log("Rock button clicked");
 })
 const paperButton =
 document.getElementById("paper")
-paperButton.addEventListener("Click", () => {
-  console.log("Paper button clicked")
+paperButton.addEventListener("click", () => {
+  console.log("Paper button clicked");
 })
 const scissorsButton =
 document.getElementById("scissors")
-paperButton.addEventListener("Click", () => {
-  console.log("Scissors button clicked")
+paperButton.addEventListener("click", () => {
+  console.log("Scissors button clicked");
 })
