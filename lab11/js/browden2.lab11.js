5e479c266f98dd0652de1d401fefa6275f9f3228
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-27 21:44:21 -0600

    2 files modified

--- lab11/js/browden2.lab11.js
+++ lab11/js/browden2.lab11.js
@@ -1,37 +1,42 @@
 function searchBooks() {
-    const query = document.getElementById("searchInput").value;
-    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`;
+  const query = encodeURIComponent(
+    document.getElementById('searchInput').value
+  );
+  const url = 'https://www.googleapis.com/books/v1/volumes?q=' + query;
 
-    fetch(url)
-        .then(response => { //fetch function returns a Promise
-            if (!response.ok) {
-                throw new Error(`Network rresponse error`);
-            }
-            return response.json();//on successful response, return JSON body
-        })
-        .then(data => {
-            displayResults(data);
-        })
-        .catch(error => {
-            console.error("Error fetching data:", error);
-        });
+  fetch(url)
+    .then((response) => {
+      //fetch function returns a Promise
+      if (!response.ok) {
+        throw new Error(`Network rresponse error`);
+      }
+      return response.json(); //on successful response, return JSON body
+    })
+    .then((data) => {
+      displayResults(data);
+    })
+    .catch((error) => {
+      console.error('Error fetching data:', error);
+    });
 }
 
 function displayResults(data) {
-    const resultsContainer = document.getElementById("results"); //Retrieves div from html file
-    resultsContainer.innerHTML = ""; //changes HTML contents of "result"
+  const resultsContainer = document.getElementById('results'); //Retrieves div from html file
+  resultsContainer.innerHTML = ''; //changes HTML contents of "result"
 
-    if (data.items) {
-        data.items.forEach(item => {
-            const volumeInfo = item.volumeInfo;
-            const title = volumeInfo.title || "Unknown Title";
-            const authors = volumeInfo.authors || ["Unknown Author"];
+  if (data.items) {
+    data.items.forEach((item) => {
+      const volumeInfo = item.volumeInfo;
+      const title = volumeInfo.title || 'Unknown Title';
+      const authors = volumeInfo.authors || ['Unknown Author'];
 
-            const resultDiv = document.createElement("div");//creates more div items for each title/author
-            resultDiv.innerHTML = `<p><strong>Title:</strong> ${title}</p><p><strong>Authors:</strong> ${authors.join(", ")}</p>`;//newly created div HTML content changed to title and authors items
-            resultsContainer.appendChild(resultDiv);//newly created divs pushed to resultsContainer
-        });
-    } else {
-        resultsContainer.innerHTML = "<p>No results found.</p>";
-    }
+      const resultDiv = document.createElement('div'); //creates more div items for each title/author
+      resultDiv.innerHTML = `<p><strong>Title:</strong> ${title}</p><p><strong>Authors:</strong> ${authors.join(
+        ', '
+      )}</p>`; //newly created div HTML content changed to title and authors items
+      resultsContainer.appendChild(resultDiv); //newly created divs pushed to resultsContainer
+    });
+  } else {
+    resultsContainer.innerHTML = '<p>No results found.</p>';
+  }
 }
