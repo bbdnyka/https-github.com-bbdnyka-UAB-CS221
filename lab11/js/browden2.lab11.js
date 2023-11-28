84ba5522afb74c58bc8cbebc49854537c31c94bb
bbdnyka <122620775+bbdnyka@users.noreply.github.com>
2023-11-27 21:05:37 -0600

    js function for google api

--- lab11/js/browden2.lab11.js
+++ lab11/js/browden2.lab11.js
@@ -1,7 +1,37 @@
-google.books.load();
+function searchBooks() {
+    const query = document.getElementById("searchInput").value;
+    const url = `https://www.googleapis.com/books/v1/volumes?q=${encodeURIComponent(query)}`;
 
-function initialize() {
-    var viewer = new google.books.DefaultViewer(document.getElementById('viewerCanvas'));
-    viewer.load('ISBN:0738531367');
+    fetch(url)
+        .then(response => { //fetch function returns a Promise
+            if (!response.ok) {
+                throw new Error(`Network rresponse error`);
+            }
+            return response.json();//on successful response, return JSON body
+        })
+        .then(data => {
+            displayResults(data);
+        })
+        .catch(error => {
+            console.error("Error fetching data:", error);
+        });
 }
-google.books.setOnLoadCallback(initialize);
+
+function displayResults(data) {
+    const resultsContainer = document.getElementById("results");
+    resultsContainer.innerHTML = "";
+
+    if (data.items) {
+        data.items.forEach(item => {
+            const volumeInfo = item.volumeInfo;
+            const title = volumeInfo.title || "Unknown Title";
+            const authors = volumeInfo.authors || ["Unknown Author"];
+
+            const resultDiv = document.createElement("div");
+            resultDiv.innerHTML = `<p><strong>Title:</strong> ${title}</p><p><strong>Authors:</strong> ${authors.join(", ")}</p>`;
+            resultsContainer.appendChild(resultDiv);
+        });
+    } else {
+        resultsContainer.innerHTML = "<p>No results found.</p>";
+    }
+}
